c32b2722d1ab2607882128885f5bd3a9d7b4d2a0
JoHyunwoo <jhw3874@gmail.com>
2024-02-22 17:18:38 +0900

    git init

---
+++ libeary-app/src/test/java/com/group/libearyapp/LibearyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.libearyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibearyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
